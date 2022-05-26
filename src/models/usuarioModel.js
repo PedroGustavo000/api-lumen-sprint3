3f47e08e50296b2d2a171622cb53a0697f6b95cf
PedroGustavo000 <pedro.jesus@sptech.school>
2022-05-26 11:15:46 -0300

    Atualização

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -12,7 +12,7 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM usuario WHERE logon = '${email}' AND senha = '${senha}';
+        SELECT * FROM Usuario WHERE logon = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -31,21 +31,13 @@ function cadastrarShopping(nomeEmp, cnpj, endereco) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-<<<<<<< HEAD
 function cadastrar(logon, senha, cpf, fone, nome, cnpj) {
-=======
-function cadastrar(logon, senha, cpf, fone, nome) {
->>>>>>> 34c7b793956313f45e067c5323602a2117179cf3
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", logon, senha, cpf, fone, nome);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-<<<<<<< HEAD
-    INSERT INTO Usuario (nome, logon, senha, cpf, telefone, fkShopping) VALUES ('${nome}', '${logon}', '${senha}', '${cpf}', '${fone}', '${cnpj}'); 
-=======
-    INSERT INTO Usuario (logon, senha, cpf, telefone, nome) VALUES ('${logon}', '${senha}', '${cpf}', '${fone}', '${nome}'); 
->>>>>>> 34c7b793956313f45e067c5323602a2117179cf3
+    INSERT INTO Usuario (nome, logon, senha, cpf, telefone, fkShopping) VALUES ('${nome}', '${logon}', '${senha}', '${cpf}', '${fone}', '${cnpj}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
