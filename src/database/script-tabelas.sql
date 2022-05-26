bab531229c6a97a1bb6840175413b644a6413681
PedroGustavo000 <pedro.jesus@sptech.school>
2022-05-26 15:38:32 -0300

    Atualizção

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,3 +1,5 @@
+-- CRIAÇÃO DE TABELAS NA AZURE
+
 create table Shopping(
 	idShopping int primary key identity(1,1),
 	nome varchar(45),
@@ -34,6 +36,50 @@ create table Sensor(
 
 create table Registro(
 	idRegistro int primary key identity(1,1),
+	medida int,
+	dataHoraRgt datetime,
+	fkSensor int,
+	foreign key (fkSensor) references Sensor(idSensor)
+);
+
+-- CRIAÇÃO DE TABELAS NA AZURE
+
+create table Shopping(
+	cnpj char(14) primary key,
+	nome varchar(45),
+	endereco varchar(45)
+);
+
+create table Usuario(
+	idUsuario int primary key auto_increment,
+    nome varchar(45),
+	logon varchar(45),
+	senha varchar(45),
+	cpf char(11),
+	telefone char(11),
+	tipo char(1),
+	fkGestor int,
+	foreign key (fkGestor) references Usuario(idUsuario),
+	fkShopping char(14),
+	foreign key (fkShopping) references Shopping(cnpj)
+);
+
+create table Setor(
+	idSetor int primary key auto_increment,
+	nome varchar(45),
+	fkShopping char(14),
+	foreign key (fkShopping) references Shopping(cnpj)
+);
+
+create table Sensor(
+	idSensor int primary key auto_increment,
+	modelo varchar(45),
+	fkSetor int,
+	foreign key (fkSetor) references Setor(idSetor)
+);
+
+create table Registro(
+	idRegistro int primary key auto_increment,
 	medida int,
 	dataHoraRgt datetime,
 	fkSensor int,
